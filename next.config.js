cb29d923cb0eb07c984e09d1462b6cc08da87001
Ronnie Dagan <59354932+RonnieDagan@users.noreply.github.com>
2024-04-20 17:30:33 -0700

    Update next.config.js to reverse proxy

--- next.config.js
+++ next.config.js
@@ -3,6 +3,14 @@ const withMakeswift = require('@makeswift/runtime/next/plugin')()
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  async rewrites() {
+    return [
+      {
+        source: '/blog',
+        destination: 'https://acme.com/blog',
+      },
+    ]
+  },
 }
 
 module.exports = withMakeswift(nextConfig)
