833114c3c31347a22cb249e4acf44262f8a2d267
Ronnie Dagan <59354932+RonnieDagan@users.noreply.github.com>
2024-04-20 17:34:10 -0700

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -6,11 +6,17 @@ const nextConfig = {
   async rewrites() {
     return [
       {
-        source: '/blog',
-        destination: 'https://acme.com/blog',
+        source: "/ingest/static/:path*",
+        destination: "https://us-assets.i.posthog.com/static/:path*",
       },
-    ]
+      {
+        source: "/ingest/:path*",
+        destination: "https://us.i.posthog.com/:path*",
+      },
+    ];
   },
+  // This is required to support PostHog trailing slash API requests
+  skipTrailingSlashRedirect: true, 
 }
 
 module.exports = withMakeswift(nextConfig)
