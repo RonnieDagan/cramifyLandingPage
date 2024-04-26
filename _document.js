ec352614654a5a9b701f8e736a8391c10cabba81
Ronnie Dagan <59354932+RonnieDagan@users.noreply.github.com>
2024-04-25 19:52:34 -0700

    Create _document.js

---
+++ _document.js
@@ -0,0 +1,21 @@
+// pages/_document.js
+import Document, { Html, Head, Main, NextScript } from 'next/document';
+
+class MyDocument extends Document {
+  render() {
+    return (
+      <Html>
+        <Head>
+          <title>cramify</title>
+          {/* Other meta tags, links, etc. */}
+        </Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    );
+  }
+}
+
+export default MyDocument;
